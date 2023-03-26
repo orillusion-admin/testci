30f8a5f29bd719312dab53d90a76eef5ab7d5d74
ShuangLiu <liu.shuang@huasheng.io>
2023-03-26 13:56:13 +0800

    test ci set exit status

--- ci/main.js
+++ ci/main.js
@@ -1,6 +1,8 @@
 const { app, BrowserWindow, ipcMain } = require('electron')
 const path = require('path')
 const {spawn} = require('child_process')
+const core = require('@actions/core');
+
 
 const HOST = 'http://localhost:4000'
 
@@ -19,15 +21,16 @@ const createWindow = async ()=>{
     ipcMain.on('log', (_event, log) => {
         console.log(log)
         
-        if(log === 'pass')
+        if(log === 'pass'){
             console.log('\x1b[32mCI pass\x1b[0m')
-        else
+        }else{
             console.error('\x1b[31mCI not pass\x1b[0m')
- 
+            core.setFailed(`CI not pass`);
+        }
         app.quit()
     })
     ipcMain.on('error', (_event, log) => {
-        console.error(log)
+        console.error(`\x1b[31m${log}\x1b[0m`)
     })
 
     await win.loadURL(HOST + '/new_test/?auto')
